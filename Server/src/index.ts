c3a939612ef13883b9fd016852a78ebc054a4673
Maxime Leriche <maxleriche.60@gmail.com>
2021-07-04 11:00:17 +0200

    [add] Connect to the database and fix .env

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -2,9 +2,16 @@ require("dotenv-flow").config({
   path: "./src/Config",
 });
 console.log("Server Startup  ", process.env.TEST);
+import Connection from "./Helper/dataBase";
 import WebServer from "./Helper/Server";
 import Api from "./Api/Api";
 export var Serveur = new WebServer(Number(process.env.PORT) || 5000);
+
+Connection.once("open", () => console.log("DatabaseOn"));
+Connection.on(
+  "error",
+  console.error.bind(console, "The database as encounter an error:")
+);
 
 Serveur.AddRouter(Api);
 
