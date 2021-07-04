cf332c1e9f527f8f4e12c5aebfc2f3c54a1fce1b
Maxime Leriche <maxleriche.60@gmail.com>
2021-07-04 18:59:09 +0200

    [fix] correction du AuthHandler

--- Server/src/Api/Auth.ts
+++ Server/src/Api/Auth.ts
@@ -18,7 +18,7 @@ var router = new Router();
 
 router
   .get("/", async (ctx: any, next: any) => {
-    var Token = ctx.get("Authentification");
+    var Token = ctx.get("Authorization").replace("Bearer ", "");
     if (validateToken(Token, false)) {
       var user = await User.findOne({
         Username: getPayloadJson(await getTokenContent(Token, false)).Username,
@@ -31,7 +31,10 @@ router
         UiColor: user.UiColor,
       };
     } else {
-      ctx.throw(401, { message: "Votre token n'est plus valide" });
+      ctx.throw(
+        401,
+        JSON.stringify({ message: "Votre token n'est plus valide" })
+      );
     }
     await next();
   })
@@ -40,10 +43,9 @@ router
     if (Username === undefined && Password === undefined) {
       ctx.throw(401, { message: "Vos identifiant ne sont pas valide" });
     } else {
-      var user = User.findOne({
+      var user = await User.findOne({
         Username,
       });
-
       if (user && checkPassword(Password, user.Password)) {
         var Token = await getToken(
           { Username: user.Username, Name: user.Name },
@@ -59,31 +61,41 @@ router
           Token: Token,
         };
       } else {
-        ctx.throw(401, { message: "No User Found" });
+        ctx.throw(
+          401,
+          JSON.stringify({ message: "No User Found or password incorrect" })
+        );
       }
     }
 
     await next();
   })
   .post("/logout", async (ctx: any, next: any) => {
-    var Token = ctx.get("Authentification");
+    var Token = ctx.get("Authorization").replace("Bearer ", "");
     if (validateToken(Token, true)) {
       var user = await User.findOne({
         Username: getPayloadJson(await getTokenContent(Token, true)).Username,
       });
-      if (user.Token.include(Token)) {
+      if (
+        user &&
+        user.Token.findIndex((value: string) => Token === value) !== -1
+      ) {
         user.Token.splice(
           user.Token.findIndex((value: string) => value === Token),
           1
         );
+        await user.save();
         ctx.body = {
           message: "Vous êtes bien déconnecter",
         };
       } else {
-        ctx.throw(401, {
-          message:
-            "Vous ne pouvez pas vous déconnecter si vous n'êtes pas connectable",
-        });
+        ctx.throw(
+          401,
+          JSON.stringify({
+            message:
+              "Vous ne pouvez pas vous déconnecter si vous n'êtes pas connectable",
+          })
+        );
       }
     } else {
       ctx.throw(401, {
@@ -103,25 +115,28 @@ router
       !validateNameSurname(Surname) ||
       !validateMail(Email)
     ) {
-      ctx.throw(401, {
-        message: "Un problème est présent sur les donnée du formulaire",
-        Username: !validateUsername(Username),
-        Password: !validatePassword(Password),
-        Name: !validateNameSurname(Name),
-        Surname: !validateNameSurname(Surname),
-        Email: !validateMail(Email),
-        AllowEmail: AllowEmail === undefined,
-      });
+      ctx.throw(
+        401,
+        JSON.stringify({
+          message: "Un problème est présent sur les donnée du formulaire",
+          Username: !validateUsername(Username),
+          Password: !validatePassword(Password),
+          Name: !validateNameSurname(Name),
+          Surname: !validateNameSurname(Surname),
+          Email: !validateMail(Email),
+          AllowEmail: AllowEmail === undefined,
+        })
+      );
     } else {
       var AUser =
         (await User.findOne({
           Username,
-        })) === undefined;
+        })) === null;
       AUser =
         AUser === true &&
         (await User.findOne({
           Email,
-        })) === undefined;
+        })) === null;
       if (AUser) {
         var user = new User({
           Username,
@@ -136,32 +151,42 @@ router
           message: "AllIsGood Please auth with the login form",
         };
       } else {
-        ctx.throw(400, { message: "A User already exist" });
+        ctx.throw(400, JSON.stringify({ message: "A User already exist" }));
       }
     }
     await next();
   })
   .post("/renew", async (ctx: any, next: any) => {
-    var Token = ctx.get("Authentification");
+    var Token = String(ctx.get("Authorization")).replace("Bearer ", "");
     if (validateToken(Token, true)) {
       var user = await User.findOne({
         Username: getPayloadJson(await getTokenContent(Token, true)).Username,
       });
-      if (user.Token.include(Token)) {
+      if (
+        user &&
+        user.Token.findIndex((value: string) => value === Token) !== -1
+      ) {
+        var payload = getPayloadJson(await getTokenContent(Token, true));
         ctx.body = {
           message: "here is your Use Token",
-          Token: getToken(
-            getPayloadJson(await getTokenContent(Token, true)),
+          Token: await getToken(
+            { Username: payload.Username, Name: payload.Name },
             false
           ),
         };
       } else {
-        ctx.throw(401, {
-          message: "Vous n'êtes plus connecter",
-        });
+        ctx.throw(
+          401,
+          JSON.stringify({
+            message: "Vous n'êtes plus connecter",
+          })
+        );
       }
     } else {
-      ctx.throw(401, { message: "Votre renew token n'est plus valide" });
+      ctx.throw(
+        401,
+        JSON.stringify({ message: "Votre renew token n'est plus valide" })
+      );
     }
     await next();
   });
