8dd911c725a16232b2518d56b69b7717a3fef904
Maxime Leriche <maxleriche.60@gmail.com>
2021-07-04 20:49:00 +0200

    [add] ajout d'un chemin Home

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Menu from "./components/Menu";
-
+import { Route } from "react-router-dom";
 const App = () => (
   <div>
     <Menu />
+    <Route exact path="/">
+      <h1>Home</h1>
+    </Route>
   </div>
 );
 
