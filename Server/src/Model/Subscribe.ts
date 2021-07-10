175a52b39a250243900964dc9df9d52d1d70205c
Maxime Leriche <maxleriche.60@gmail.com>
2021-07-10 14:25:58 +0200

    [Add] user model and ForCompteur Model

--- Server/src/Model/Subscribe.ts
+++ Server/src/Model/Subscribe.ts
@@ -4,8 +4,9 @@ const SubscribeCompteurSchema = new mongoose.Schema({
   Owner: Boolean, // Tout les droit sur le Compteur
   SharedOwner: Boolean, //Ne peux pas supprimer le compteur
   Notification: Boolean, // si l'utilisateur autorise les notification push
-  NotificationSub: String, // Le Payload qui permet de send les notification push
+  NotificationSub: Boolean, // Le Payload qui permet de send les notification push
   CompteurId: String,
+  UserId: String,
 });
 
 export default mongoose.model("SubCompteur", SubscribeCompteurSchema);
