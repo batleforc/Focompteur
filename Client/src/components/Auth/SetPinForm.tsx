663d74d8be870037ea35d3514a098f15535e0e28
Maxime Leriche <maxleriche.60@gmail.com>
2021-07-14 18:23:28 +0200

    [fix] style of pinform

--- Client/src/components/Auth/SetPinForm.tsx
+++ Client/src/components/Auth/SetPinForm.tsx
@@ -37,7 +37,7 @@ const PinForm = ({
               value={Pin}
               onChange={(e) => setPin(e.currentTarget.value)}
               required
-              className="form-input appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              className="form-input appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder={t("Pin")}
             />
           </div>
