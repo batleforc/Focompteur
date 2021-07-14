5814aabb7a44d2ec745aca7cf9efd8b1c562c252
Maxime Leriche <maxleriche.60@gmail.com>
2021-07-14 18:19:29 +0200

    [add] new icon sys to register and new logic to Login

--- Client/src/pages/Register.tsx
+++ Client/src/pages/Register.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import { RootState } from "../action";
+import { AppDispatch, RootState } from "../action";
 import { useTranslation } from "react-i18next";
-const Register = () => {
+import Icon from "../components/Icon";
+const Register = ({
+  dispatch,
+  AuthPending,
+}: {
+  dispatch: AppDispatch;
+  AuthPending: Boolean;
+}) => {
   const { t } = useTranslation();
   return (
     <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -109,19 +116,22 @@ const Register = () => {
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <svg
-                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <Icon
+                  className={
+                    !AuthPending
+                      ? "h-5 w-5 fill-current text-indigo-500 group-hover:text-indigo-400"
+                      : "hidden"
+                  }
+                  Label="lock"
+                />
+                <Icon
+                  className={
+                    AuthPending
+                      ? "animate-spin -ml-1 mr-3 h-5 w-5 fill-current text-indigo-500"
+                      : "hidden"
+                  }
+                  Label="hour-glass"
+                />
               </span>
               {t("Register")}
             </button>
@@ -131,4 +141,6 @@ const Register = () => {
     </div>
   );
 };
-export default connect((state: RootState) => ({}))(Register);
+export default connect(({ Auth }: RootState) => ({
+  AuthPending: Auth.Pending,
+}))(Register);
